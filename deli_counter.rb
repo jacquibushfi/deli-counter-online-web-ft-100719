4e34988174ce4f1acdb3511e203b0a758e6fc0f7
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-08 13:34:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,8 @@ def line(place)
    place.each.with_index(1) do |name, index|
      line_array.push("#{index}.   #{name}")
   end    
- end
   puts "The line is currently: "#{line_array.join(" ")}"
+end
 end
 
 def take_a_number(array, name)
