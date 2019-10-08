0620422948961c27ec3b12863c62a90fd2b2b5a9
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-08 12:56:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
-def line() 
+def line(array,name) 
   katz_deli = []
  if katz_deli = nil 
-   puts "There is nobody waiting to be served!"
+   puts "The line is currently empty"
+ else
+   puts "#{name} is number #katz_deli"
 end
 end
